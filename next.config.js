cd2c1f51563f227a587cc85e0e5c87116585e0c9
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-06-27 23:40:16 -0600

    pwa support

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+
+import withPWA from 'next-pwa'
+
+
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io','images.unsplash.com'],
   },
+  pwa: {
+    dest: 'public', // this will be used to output service-worker.js
+    register: true,
+    skipWaiting: true,
+    offlinePage: '/offline', // add the route to your offline page
+  },
 }
 
-module.exports = nextConfig
+
+export default nextConfig
---
+++ pages/offline.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const offline = () => {
+  return (
+    <div>offline</div>
+  )
+}
+
+export default offline
