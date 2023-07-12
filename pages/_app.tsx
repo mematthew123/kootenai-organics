53b65de693d599da43f08c3788a5f326b50d2f70
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-12 17:31:52 -0600

    cdn= ture added ana;lytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,22 @@ import { AppProps } from "next/app";
 import { useState } from "react";
 import blurStyles from "../components/blur.module.css";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
+    <>
       <ModalContext.Provider value={{ isOpen, setIsOpen }}>
         <AgeVerifyModal />
         {/* <Navbar /> */}
         <div className={isOpen ? blurStyles.blur : ""}>
           {/* <Layout> */}
-            <Component {...pageProps} />{" "}
-          {/* </Layout> */}
+          <Component {...pageProps} /> {/* </Layout> */}
         </div>
       </ModalContext.Provider>
+      <Analytics />
+    </>
   );
 }
