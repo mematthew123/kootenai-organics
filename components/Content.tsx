1608f0db56f37132187b49e8b4b3c79fe0ace6a8
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-11 23:30:22 -0600

    added bullets to portable text

--- components/Content.tsx
+++ components/Content.tsx
@@ -5,6 +5,7 @@ import { Fraunces, Poppins } from "next/font/google";
 import SectionUp from "@/animations/sectionUp";
 import { CameraIcon } from "@heroicons/react/20/solid";
 import { PortableText } from "@portabletext/react";
+import styles from "./Content.module.css";
 
 const inter = Fraunces({
   subsets: ["latin"],
@@ -20,7 +21,6 @@ const poppins = Poppins({
   weight: "200",
 });
 
-
 interface MainContentData {
   title: string;
   description: string;
@@ -35,15 +35,12 @@ const Content: React.FC<{ mainContentData: MainContentData }> = ({
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-
-
-
   return (
     <>
-      <div className="bg-[#E9EDC9] mt-20 lg:mt-40 flex flex-col lg:flex-row items-center p-4 lg:p-10 space-y-4 lg:space-y-0 lg:space-x-4 lg:w-[1100px] max-w-full mx-auto mb-20 rounded-md overflow-hidden shadow-lg border border-gray-200">
-        <div className="relative mx-auto max-w-7xl px-6 py-16 lg:px-8">
-          <div className="absolute bottom-0 left-3/4 top-0 hidden w-screen bg-gray-50 lg:block" />
-          <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
+      <div className='bg-[#E9EDC9] mt-20 lg:mt-40 flex flex-col lg:flex-row items-center p-4 lg:p-10 space-y-4 lg:space-y-0 lg:space-x-4 lg:w-[1100px] max-w-full mx-auto mb-20 rounded-md overflow-hidden shadow-lg border border-gray-200'>
+        <div className='relative mx-auto max-w-7xl px-6 py-16 lg:px-8'>
+          <div className='absolute bottom-0 left-3/4 top-0 hidden w-screen bg-gray-50 lg:block' />
+          <div className='mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8'>
             <div>
               <h3
                 className={`${inter.className} mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl`}
@@ -52,70 +49,67 @@ const Content: React.FC<{ mainContentData: MainContentData }> = ({
               </h3>
             </div>
           </div>
-          <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
-            <div className="relative lg:col-start-2 lg:row-start-1">
+          <div className='mt-8 lg:grid lg:grid-cols-2 lg:gap-8'>
+            <div className='relative lg:col-start-2 lg:row-start-1'>
               <svg
-                className="absolute right-0 top-0 -mr-20 -mt-20 hidden lg:block"
+                className='absolute right-0 top-0 -mr-20 -mt-20 hidden lg:block'
                 width={404}
                 height={384}
-                fill="none"
-                viewBox="0 0 404 384"
-                aria-hidden="true"
+                fill='none'
+                viewBox='0 0 404 384'
+                aria-hidden='true'
               >
                 <defs>
                   <pattern
-                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                    id='de316486-4a29-4312-bdfc-fbce2132a2c1'
                     x={0}
                     y={0}
                     width={20}
                     height={20}
-                    patternUnits="userSpaceOnUse"
+                    patternUnits='userSpaceOnUse'
                   >
                     <rect
                       x={0}
                       y={0}
                       width={4}
                       height={4}
-                      className="text-gray-200"
-                      fill="currentColor"
+                      className='text-gray-200'
+                      fill='currentColor'
                     />
                   </pattern>
                 </defs>
                 <rect
                   width={404}
                   height={384}
-                  fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
+                  fill='url(#de316486-4a29-4312-bdfc-fbce2132a2c1)'
                 />
               </svg>
-              <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
+              <div className='relative mx-auto max-w-prose text-base lg:max-w-none'>
                 <figure>
-                  <div className="aspect-h-7 aspect-w-12 lg:aspect-none">
+                  <div className='aspect-h-7 aspect-w-12 lg:aspect-none'>
                     <Image
-                      className="rounded-lg object-cover object-center shadow-lg"
+                      className='rounded-lg object-cover object-center shadow-lg'
                       src={mainContentData.mainImage}
-                      alt="Whitney leaning against a railing on a downtown street"
+                      alt='Whitney leaning against a railing on a downtown street'
                       width={1184}
                       height={1376}
                     />
                   </div>
-                  <figcaption className="mt-3 flex text-sm text-gray-600">
+                  <figcaption className='mt-3 flex text-sm text-gray-600'>
                     <CameraIcon
-                      className="h-5 w-5 flex-none text-gray-400"
-                      aria-hidden="true"
+                      className='h-5 w-5 flex-none text-gray-400'
+                      aria-hidden='true'
                     />
-                    <span className="ml-2">Photograph by Marcus O’Leary</span>
+                    <span className='ml-2'>Photograph by Marcus O’Leary</span>
                   </figcaption>
                 </figure>
               </div>
             </div>
-            <div className="mt-8 lg:mt-0">
+            <div className='mt-8 lg:mt-0'>
               <SectionUp>
-              <div
-              className={
-                poppins.className +
-                "  text-lg leading-relaxed mx-auto mb-8   max-w-prose text-gray-600  lg:max-w-none"
-              }
-            >
+                <div
+                  className={`${poppins.className} text-lg leading-relaxed mx-auto mb-8 space-y-10 max-w-prose text-gray-600  lg:max-w-none ${styles.portableText}`}
+                >
                   <PortableText value={mainContentData.body} />
                 </div>
                 {/* </div> */}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,29 +38,35 @@ const Hero: React.FC<{ heroData: HeroData }> = ({ heroData }) => {
     <div
       className={` ${textColor}  max-w-[1240px] mb-40 h-screen flex flex-col justify-center items-center p-4 rounded-lg`}
     >
-      <div className=" flex flex-col justify-center items-center text-center sm:items-start space-y-6">
+      <div className=' flex flex-col justify-center items-center text-center sm:items-start space-y-6'>
         <img
           src={heroData.heroImage}
           alt={heroData.alt}
-          className="absolute inset-0 object-fit min-h-screen aspect-auto w-full z-0"
-          draggable="false"
+          className='absolute inset-0 object-fit min-h-screen aspect-auto w-full z-0'
+          draggable='false'
         />
         <div
           style={{ color: textColor }}
           className={` m-4 absolute inset-0 flex flex-col text-center p-20 small:text-left small:p-32 ${positionClass}`}
         >
-          <h2 className="text-4xl mb-4">{heroData.title}</h2>
-          <p className=" flex font-extrabold text-xl text-center align-middle left-50% top-50%  text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          <h2 className='text-4xl mb-4'>{heroData.title}</h2>
+          <p className=' flex font-extrabold text-xl text-center align-middle left-50% top-50%  text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black'>
             {heroData.description}
           </p>
-          <button className="flex items-center justify-center w-40 h-12 bg-primary-500 rounded-full text-white text-base-regular font-bold drop-shadow-md shadow-black">
+          <button
+            style={{ color: textColor }}
+            className='flex items-center justify-center w-40 h-12 bg-primary-500 rounded-full text-white text-base-regular font-bold drop-shadow-md shadow-black'
+          >
             Shop Now
           </button>
         </div>
-        <Link href="/menu">
-          <div className="flex items-center text-large-regular border-b border-current gap-x-4 py-2 transition-all duration-300 group hover:pl-4 hover:pr-1">
+        <Link href='/menu'>
+          <div
+            style={{ color: textColor }}
+            className='flex items-center text-large-regular border-b border-current gap-x-4 py-2 transition-all duration-300 group hover:pl-4 hover:pr-1'
+          >
             <span>Check it out</span>
-            <ArrowLongRightIcon className="w-5 h-5 group-hover:transform group-hover:translate-x-1 transition-all duration-300" />
+            <ArrowLongRightIcon className='w-5 h-5 group-hover:transform group-hover:translate-x-1 transition-all duration-300' />
           </div>
         </Link>
       </div>
