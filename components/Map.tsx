0bd796e75fd0d891142e005ece3a2a5ae27039e8
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-19 09:24:24 -0600

    simplified the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,4 @@
-import {
-  GoogleMap,
-  LoadScript,
-  MarkerF,
-  InfoWindow,
-} from "@react-google-maps/api";
-import React, { Key, useState } from "react";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
 const mapStyles = {
   height: "100%",
@@ -18,43 +12,11 @@ const defaultCenter = {
   lng: -113.9931,
 };
 
-export interface LocationsProps {
-  locations: {
-    _id: Key | null | undefined;
-    name: string;
-    description: string;
-    locations: {
-      lat: number;
-      lng: number;
-      _key: Key | null | undefined;
-    }[];
-  }[];
-}
-
 const defaultMapOptions = {
   disableDefaultUI: true,
 };
 
-const Map: React.FC<LocationsProps> = ({ locations }) => {
-  const [selectedLocation, setSelectedLocation] = useState<{
-    name: string;
-    description: string;
-    locations: {
-      lat: number;
-      lng: number;
-      _key: Key | null | undefined;
-    }[];
-  } | null>({
-    name: "The Missoula Childrens Theater",
-    description: "Description of The Missoula Childrens Theater", // Update with your description
-    locations: [
-      {
-        lat: 46.8746,
-        lng: -113.9931,
-        _key: "missoula_theater",
-      },
-    ],
-  });
+const SimpleMap = () => {
   return (
     <div className='mt-10 mb-10 mx-auto h-[50vh] w-full md:w-[50vw] rounded-lg overflow-hidden'>
       <LoadScript
@@ -66,40 +28,11 @@ const Map: React.FC<LocationsProps> = ({ locations }) => {
           center={defaultCenter}
           options={defaultMapOptions}
         >
-          {selectedLocation && (
-            <MarkerF
-              key={selectedLocation.locations[0]._key}
-              position={{
-                lat: selectedLocation.locations[0].lat,
-                lng: selectedLocation.locations[0].lng,
-              }}
-              onClick={() => {
-                setSelectedLocation(null);
-              }}
-            />
-          )}
-          {selectedLocation && (
-            <InfoWindow
-              position={{
-                lat: selectedLocation.locations[0].lat,
-                lng: selectedLocation.locations[0].lng,
-              }}
-              onCloseClick={() => {
-                setSelectedLocation(null);
-              }}
-            >
-              <div>
-                <h2 className='text-[24px] font-bold'>
-                  {selectedLocation.name}
-                </h2>
-                <p className='text-[16px]'>{selectedLocation.description}</p>
-              </div>
-            </InfoWindow>
-          )}
+          <MarkerF position={defaultCenter} />
         </GoogleMap>
       </LoadScript>
     </div>
   );
 };
 
-export default Map;
+export default SimpleMap;
