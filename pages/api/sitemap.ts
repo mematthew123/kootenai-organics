2dc449681da263f61f0937d5245c2a635d007a1a
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-19 12:12:05 -0600

    commented out sitemap file

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -2,22 +2,22 @@
 
 
 
-export default function handler(req: any, res: { statusCode: number; setHeader: (arg0: string, arg1: string) => void; end: (arg0: string) => void }) {
+// export default function handler(req: any, res: { statusCode: number; setHeader: (arg0: string, arg1: string) => void; end: (arg0: string) => void }) {
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/xml')
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/xml')
       
-      // Instructing the Vercel edge to cache the file
-      res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
+//       // Instructing the Vercel edge to cache the file
+//       res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
       
-      // generate sitemap here
-      const xml = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-      <url>
-        <loc>https://www.kootenaiorganics.com/</loc>
-        <lastmod>2023-06-27</lastmod>
-      </url>
-      </urlset>`
+//       // generate sitemap here
+//       const xml = `<?xml version="1.0" encoding="UTF-8"?>
+//       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+//       <url>
+//         <loc>https://www.kootenaiorganics.com/</loc>
+//         <lastmod>2023-06-27</lastmod>
+//       </url>
+//       </urlset>`
   
-    res.end(xml)
-  }
+//     res.end(xml)
+//   }
