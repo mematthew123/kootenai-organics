9378ef727aa3a533cce9fe514242c76362847546
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-21 21:01:02 -0600

    imported fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
-        'fraunces': ['Fraunces', 'serif'],
+        poppins: ["Poppins", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
+        ElCaminoSoftEdges: ["El Camino", "regular"],
       },
     },
   },
   plugins: [],
-}
+};
