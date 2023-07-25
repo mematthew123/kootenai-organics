7448eb478cae720a57afc0e3c4ae92ae392b3414
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-07-24 18:41:15 -0600

    replaced _document images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,28 +2,37 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
         {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-        <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/canopy.jpg" />
-        <link rel="apple-touch-icon" href="/canopy.jpg" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
+        <meta charSet='utf-8' />
+        <link
+          rel='shortcut icon'
+          href='/Kootenai Organics_Primary Logo - Grass.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          href='/Kootenai Organics_Primary Logo - Grass.png'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='theme-color' content='#000000' />
         <meta
-          name="description"
+          name='description'
           content="Missoula's premier organic cannabis and lifestyle brand, selling organic and sustainable cannabis products."
         />
-        <meta property="og:title" content="Kootenai Organics" key="title" />
+        <meta property='og:title' content='Kootenai Organics' key='title' />
         <meta
-          property="og:description"
+          property='og:description'
           content=" Missoula's premier organic cannabis and lifestyle brand"
         />
-        <meta property="og:image" content="/canopy.jpg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.kootenaiorganics.com" />
+        <meta
+          property='og:image'
+          content='/Kootenai Organics_Primary Logo - Grass.png'
+        />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.kootenaiorganics.com' />
       </Head>
       <body>
         <Main />
